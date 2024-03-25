39877e6beff740afe52d26dad10f81de9b1124cb
donlHenard <pastor.git101@gmail.com>
2024-03-25 17:32:31 +0800

    add size function

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ console.log(list.head().getValue());
 list.prepend("test");
 console.log(list.head().getValue());
 list.prepend("trial");
+console.log(list.size());
 const body = document.querySelector('body');
 body.textContent = list.toString();
--- src/linkedList.js
+++ src/linkedList.js
@@ -23,6 +23,19 @@ const CreateLinkedList = () => {
             newNode.setNext(_headNode);
             _headNode = newNode;
         },
+        size: () => {
+            let iteration = 0;
+            const tailCheck = (node) => {
+                if(node.getNext() == null) {
+                    iteration += 1;
+                } else {
+                    iteration += 1;
+                    tailCheck(node.getNext());
+                }
+            }
+            tailCheck(_headNode);
+            return iteration;
+        },
         head: () => _headNode,
         toString: () => {
             let result = "";
@@ -35,8 +48,8 @@ const CreateLinkedList = () => {
                     
                 }
             }
-        tailCheck(_headNode);
-        return result;
+            tailCheck(_headNode);
+            return result;
         }
     }
 }
