5ffeefe156666e3857d36a4c04682c8c6edb4037
donlHenard <pastor.git101@gmail.com>
2024-03-25 18:06:38 +0800

    add display for every function necessary? no. i just want to see it directly on html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import { CreateLinkedList } from "./linkedList";
-
+import { createPara } from "./others";
+//create list
 const list = CreateLinkedList();
+const listCreate = createPara('create list');
+const listResult = createPara(list.toString());
+//append
 list.append("sample");
-console.log(list.head().getValue());
+const append = createPara('append "sample"');
+const appendResult = createPara(list.toString());
+//prepend
 list.prepend("test");
-console.log(list.head().getValue());
-list.prepend("trial");
-console.log(list.size());
+const prepend = createPara('prepend "test"');
+const prependResult = createPara(list.toString());
+//size
+const sizeResult = createPara('size: ' + list.size());
+//head
+const headResult = createPara('head: ( ' + list.head().getValue() + ' )');
+//display
 const body = document.querySelector('body');
-body.textContent = list.toString();
+body.append(listCreate,listResult,
+            append,appendResult,
+            prepend,prependResult,
+            sizeResult,
+            headResult);   
